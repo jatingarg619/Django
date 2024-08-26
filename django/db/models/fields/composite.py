b401ebcfc5da395a6d9cc98aa06b4d60e60b236b
Bendeguz Csirmaz <csirmazbendeguz@gmail.com>
2024-08-26 11:58:01 +0800

    Fixed #373 - Fix.

--- django/db/models/fields/composite.py
+++ django/db/models/fields/composite.py
@@ -26,14 +26,15 @@ class CompositeAttribute:
 
     def __set__(self, instance, values):
         attnames = self.attnames
+        length = len(attnames)
 
         if values is None:
-            values = (None,) * len(attnames)
+            values = (None,) * length
 
         if not isinstance(values, (list, tuple)):
             raise ValueError(f"{self.field.name!r} must be a list or a tuple.")
-        if len(attnames) != len(values):
-            raise ValueError(f"{self.field.name!r} must have {len(attnames)} elements.")
+        if length != len(values):
+            raise ValueError(f"{self.field.name!r} must have {length} elements.")
 
         for attname, value in zip(attnames, values):
             setattr(instance, attname, value)
